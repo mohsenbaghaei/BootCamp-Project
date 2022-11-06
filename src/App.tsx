d7a88b619b1b20a3a10d9b0eb4d2d2b6100d90c4
mohsenbaghaei <111972399+mohsenbaghaei@users.noreply.github.com>
2022-11-06 14:55:08 +0330

    createing components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import { Route , Routes } from 'react-router-dom'
+import Main from './components/pages/Main'
+import Quran from './components/pages/quran/Quran'
 
 const App : React.FC = () => {
   return (
     <div>
-
+      <Routes>
+        <Route path = '/' element={<Main/>} />
+        <Route path = '/quran' element={<Quran/>} />
+      </Routes>
     </div>
   );
 }
