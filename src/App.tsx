376d7768b2eef95fadbf1a3127ba97ec353752cb
mohsenbaghaei <111972399+mohsenbaghaei@users.noreply.github.com>
2022-11-08 02:07:58 +0330

    creating

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './App.css'
 import { Route , Routes } from 'react-router-dom'
-import Main from './components/pages/Main'
+import Main from './components/pages/main/Main'
 import Quran from './components/pages/quran/Quran'
 
 const App : React.FC = () => {
   return (
-    <div>
+    <div className='container'>
       <Routes>
-        <Route path = '/' element={<Main/>} />
-        <Route path = '/quran' element={<Quran/>} />
+        {/* <Route path = '/' element={<Main/>} /> */}
+        <Route path = '/' element={<Quran/>} />
       </Routes>
     </div>
   );
--- src/components/pages/quran/Quran.tsx
+++ src/components/pages/quran/Quran.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Quran :React.FC = () => {
     return (
         <div>
-            
+            salam
         </div>
     );
 };
